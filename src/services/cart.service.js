134718f4b2f10c5ce9c34d8da6dc1e6c49fbb9ca
Pardeep-Chaurasiya <chaurasiyapardeep001@gmail.com>
2024-01-10 19:05:44 +0530

    added findUserCart serive

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -1,4 +1,5 @@
 const Cart = require("../models/cart.model")
+const CartItem = require("../models/cartItem.mode")
 
 const createCart = async(user) =>{
     try {
@@ -10,4 +11,34 @@ const createCart = async(user) =>{
     }
 }
 
-module.exports = {createCart}
+
+const findUserCart = async(userId) =>{
+    try {
+        const cart = await Cart.findOne({user:userId})
+
+        const cartItems = await CartItem.find({cart:cart._id}).populate("product")
+
+        cart.cartItems = cartItems
+
+        let totalPrice = 0;
+        let totalDiscountedPrice = 0;
+        let totalItem = 0
+
+        for(let cartItem of cart.cartItems){
+            totalPrice+=cartItem.price;
+            totalDiscountedPrice+=cartItem.discountedPrice;
+            totalItem += cartItem.quantity;
+        }
+
+        cart.totalPrice = totalPrice
+        cart.totalItem = totalItem
+        cart.totalPrice = totalPrice
+
+        return cart
+    } catch (error) {
+        throw new Error(error.message)
+        
+    }
+}
+
+module.exports = {createCart,findUserCart}
